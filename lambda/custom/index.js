5e6a501b9a2877bc4a210842577cfeb8bec63b8a
mizokami <r.mizokami@gmail.com>
2018-03-11 15:50:30 +0900

    Add minimal implementation

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,54 +1,51 @@
 'use strict';
+
 var Alexa = require("alexa-sdk");
+
+var Sound = require('find-my-iphone');
 
 var APP_ID = process.env.APP_ID;
 
+var APPLE_ID = process.env.APPLE_ID;
+
+var PASSWORD = process.env.PASSWORD;
+
+var DEVICE_NAME = process.env.DEVICE_NAME;
+
 exports.handler = function(event, context) {
   var alexa = Alexa.handler(event, context);
+
   alexa.appId = APP_ID;
+
   alexa.registerHandlers(handlers);
+
   alexa.execute();
 };
 
 var handlers = {
   'LaunchRequest': function () {
-    this.emit('SayHello');
+    this.emit('AMAZON.HelpIntent');
   },
-  'HelloWorldIntent': function () {
-    this.emit('SayHello');
-  },
-  'MyNameIsIntent': function () {
-    this.emit('SayHelloName');
-  },
-  'SayHello': function () {
-    this.response.speak('Hello World!')
-      .cardRenderer('hello world', 'hello world');
-    this.emit(':responseReady');
-  },
-  'SayHelloName': function () {
-    var name = this.event.request.intent.slots.name.value;
-    this.response.speak('Hello ' + name)
-      .cardRenderer('hello world', 'hello ' + name);
-    this.emit(':responseReady');
-  },
-  'SessionEndedRequest' : function() {
-    console.log('Session ended with reason: ' + this.event.request.reason);
-  },
-  'AMAZON.StopIntent' : function() {
-    this.response.speak('Bye');
-    this.emit(':responseReady');
-  },
+
   'AMAZON.HelpIntent' : function() {
-    this.response.speak("You can try: 'alexa, hello world' or 'alexa, ask hello world my" +
-      " name is awesome Aaron'");
-    this.emit(':responseReady');
+    this.emit(':ask', '誰のアイフォンを鳴らしますか。');
   },
-  'AMAZON.CancelIntent' : function() {
-    this.response.speak('Bye');
-    this.emit(':responseReady');
-  },
-  'Unhandled' : function() {
-    this.response.speak("Sorry, I didn't get that. You can try: 'alexa, hello world'" +
-      " or 'alexa, ask hello world my name is awesome Aaron'");
+
+  // TODO: 子供が勝手にこのインテントを発火させない仕組みを入れる
+  'SoundIPhoneByName': function () {
+    var name = this.event.request.intent.slots.name.value;
+
+    // FIXME: Apple account should be decided by `name`.
+    var appleId = APPLE_ID;
+
+    var password = PASSWORD;
+
+    var deviceName = DEVICE_NAME;
+
+    Sound(appleId, password, deviceName, function() {
+      console.log("Sounding " + name + "'s phone.");
+
+      this.emit(':tell', name + 'のアイフォンを鳴らしています。');
+    });
   }
 };
